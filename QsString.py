73cb3cbed257ac1bacd0e28d7df6282fdc99a611
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-05 11:52:42 +0530

    Add files via upload

---
+++ QsString.py
@@ -0,0 +1,52 @@
+#remove i'th character from the string in python
+'''strs = "brainmentors...123..skillrisers"
+print(strs.replace("123",""))'''
+
+#Reverse words in a given string
+'''val = "This is the python programming.."
+string = val.split()
+string = string[::-1]
+print("-".join(string))'''
+
+#to check whether the string is palindrome or not
+
+'''strs = "ababa"
+rev = strs[::-1]
+if strs == rev:
+    print("string is palindrome..")
+else:
+    print("string is not palindrome..")'''
+
+# swap commas and dots in a string
+string = "brai,n  me,  ntors.ski.  ll,risers."
+string = string.replace(",","third")
+string = string.replace(".",",")
+string = string.replace("third",".")
+print(string)
+
+#program to print even length words in a string..
+
+strs = "This is the python language"
+output = This
+         is
+         python
+        language
+# string slicing - to rotate a string by k
+strs = "brainmentors"
+k = 2
+left_rotate = "ainmentorsbr"
+right_rotate = "rsbrainmento"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
