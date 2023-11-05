16e6a2547fc260d350d90454e5be1be4fdc86fe0
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-05 11:56:09 +0530

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,23 @@
+for i in range(5):
+    print(i)
+    print("Inside the loop..")
+print("Loop exit")
+
+
+print("*************")
+
+for i in range(1,11):
+    print(i)
+    
+print("*************")  
+
+for i in range(2,21,2):
+    print(i)
+
+
+print("*************")
+
+for i in range(10,0,-1):
+    print(i)
+
+print("*************")
