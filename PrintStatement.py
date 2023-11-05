c728dd5187a85b2aed4ea6488fd8c42218b60d9c
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-05 11:58:16 +0530

    Add files via upload

---
+++ PrintStatement.py
@@ -0,0 +1,14 @@
+x = 10
+y = 3
+z = x/y
+print(z)
+print("division of x and y is z")
+
+print("division of",x,"and",y,"is",z)
+
+print("sum of %d and %d is %f"%(x,y,z))
+#if value is in int then %d
+print("division of {} and {} is {:.2f}".format(x,y,z)
+
+import keyword
+print(keyword.kwlist)
