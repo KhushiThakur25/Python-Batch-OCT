8a0ac2ab07095b38b198f6b49c4ff9566cc63bf6
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-05 11:55:32 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,22 @@
+#num = int(input("Enter the number"))
+'''
+if num > num_2:
+    print("num is greater ")
+else:
+    print("num_2 is greater")'''
+
+'''
+result = "Even" if num %2 == 0 else "odd"
+print(result,"number")'''
+
+x,y,z = 3,5,1
+'''
+if x>y and x>z :
+    print("X is greatest")
+elif y>x and y>z:
+    print("Y is greatest")
+else:
+    print("Z is greatest")'''
+
+res = "X" if x>y and x>z else "Y" if y>x and y>z else "Z"
+print(res,"is greatest")
