ffa495cb30d725789a6661a0273f908c120ef4ff
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-05 11:51:49 +0530

    Add files via upload

---
+++ strClass.py
@@ -0,0 +1,17 @@
+#methods of string
+st = "this is tHe Python coUrse"
+'''1.lower
+2.upper
+3.capitalize
+4.title
+5.swapcase
+6.casefold'''
+print(st.lower())
+print(st.upper())
+print(st.capitalize())
+print(st.title())
+print(st.swapcase())
+print(st.count("s",6))
+print(st.index("i"))
+print(st.rindex("i"))
+print(st.find("i"))
