14254b8e6d7daad4a14327f3f8812ada7997523b
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-07 15:03:34 +0530

    Add files via upload

---
+++ UppercaseQS.py
@@ -0,0 +1,13 @@
+#Uppercase half string
+'''s = "This Is A python language"
+s1 = s[:len(s)//2]
+s2 = s[len(s)//2:]
+s1 = s1.lower()
+s2 = s2.upper()
+print(s1+s2)'''
+s="This Is A python language"
+first1=s[:13]
+print(first1.upper())
+
+secon2=s[13:]
+print(secon2.lower())
